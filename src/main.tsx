12457861b404a689522d00d75762b26165c41ee8
Mehedi Hasan <mehedixhasan12@gmail.com>
2025-09-18 08:28:56 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import "./lib/firebase";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import './lib/firebase'   // keep your Firebase setup
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
